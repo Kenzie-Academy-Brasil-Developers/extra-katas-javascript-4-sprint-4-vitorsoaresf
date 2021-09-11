eed70b424bcd641744c2f53ddc91d4e959ac9a15
vitorsoaresf <vitorsoaresf@gmail.com>
2021-09-11 06:35:56 -0300

    implementa kata1 e kata2

--- katas4.js
+++ katas4.js
@@ -1,13 +1,53 @@
 const gotCitiesCSV = "King's Landing,Braavos,Volantis,Old Valyria,Free Cities,Qarth,Meereen";
 
-const lotrCitiesArray = ["Mordor","Gondor","Rohan","Beleriand","Mirkwood","Dead Marshes","Rhun","Harad"];
+const lotrCitiesArray = ["Mordor", "Gondor", "Rohan", "Beleriand", "Mirkwood", "Dead Marshes", "Rhun", "Harad"];
 
 const bestThing = "The best thing about a boolean is even if you are wrong you are only off by a bit";
 
-function kata1() {
-    // implemente aqui o código
+const showResults = (element) => {
+    const box_result = document.getElementById('box-result');
+    box_result.innerText = '';
+    box_result.innerText = element;
 }
 
-// kata1();
+const kata1 = () => {
+    let arrbestThing = bestThing.split(" ");
+    showResults(arrbestThing.indexOf('only'));
+    return arrbestThing.indexOf('only');
+}
 
-// daqui em diante é contigo
+
+const kata2 = () => {
+    let arrbestThing = bestThing.split(" ");
+    showResults(arrbestThing.indexOf(arrbestThing[arrbestThing.length - 1]));
+    return arrbestThing.indexOf(arrbestThing[arrbestThing.length - 1]);
+}
+
+const kata3 = () => {
+    let arrbestThing = bestThing.split(" ");
+    let arrFilter = arrbestThing.filter(element => 'aa'.search(element) === -1 ? 1 : element);
+    console.log(arrFilter);
+    showResults(arrFilter);
+    return arrFilter;
+}
+
+// FUNCAO QUE DETERMINARA QUAL KATA SELECIONAR SEGUNDO O ID DO BOTAO CLICADO
+// UTILIZANDO O CONCEITO DE BUBBLING EM QUE O EVENTO EH ADICIONADO AO PAI
+const kataSelect = (event) => {
+    const kata = event.target;
+    console.log(kata.id)
+    switch (kata.id) {
+        case '1':
+            kata1();
+            break;
+        case '2':
+            kata2();
+            break;
+        case '3':
+            kata3();
+            break;
+    }
+}
+
+const ulParent = document.getElementById('list-katas');
+ulParent.addEventListener('click', kataSelect);
